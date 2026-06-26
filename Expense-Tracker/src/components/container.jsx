7a92047104c20f86643b9dd57c9f0e65c1a97def
Dishant Girase <dishantgirase6@gmail.com>
2026-06-26 11:27:31 +0530

    god some errors

--- Expense-Tracker/src/components/container.jsx
+++ Expense-Tracker/src/components/container.jsx
@@ -1,7 +1,7 @@
 import styles from "./container.module.css";
 const Container = (props) => {
   return (
-    <div className={`container ${styles.custom - style}`}>{props.children}</div>
+    <div className={`container ${styles.customStyle}`}>{props.children}</div>
   );
 };
 export default Container;
